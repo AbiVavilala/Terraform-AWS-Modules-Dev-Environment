eb29f2f9b98e73efd73be1cfc3f7400590133fa7
Abilabs <Abilash6891@gmail.com>
2023-10-12 15:32:27 +1100

    changes to module

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 module "FlaskVPC" {
 source    = "./modules/VPC"
 vpc_cidr  = "10.200.0.1/16"
+pub_cidrs = ["10.200.0.0/24", "10.200.1.0/24"]
+pri_cidrs = ["10.200.2.0/24", "10.200.3.0/24"]
+
 }
 
 module "Flaskec2" {
--- modules/VPC/vars.tf
+++ modules/VPC/vars.tf
@@ -1,6 +1,5 @@
 variable "vpc_cidr" {
-    type = string
-    default = "10.200.0.0/16"
+     
   }
 
 variable "vpc_tags" {
@@ -12,10 +11,9 @@ variable "vpc_tags" {
 }
 
 variable "pub_cidrs" {
- default  = ["10.200.0.0/24", "10.200.1.0/24"]
   
 }
  
 variable "pri_cidrs" {
-  default = ["10.200.2.0/24", "10.200.3.0/24"]
+   
 }
